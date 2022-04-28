1780537a4c259ced3be4797761eca76ac05bcd91
Neoshrimp <shadow638@protonmail.com>
2022-04-29 02:33:22 +0300

    Add Shield of Retribution changes

---
+++ src/Character-rebalance/CharPatches/IronheartPatches.cs
@@ -0,0 +1,97 @@
+﻿using BepInEx;
+using GameDataEditor;
+using HarmonyLib;
+using I2.Loc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Character_rebalance.CharPatches
+{
+    class IronheartPatches
+    {
+        [HarmonyPatch(typeof(GDECharacterData), nameof(GDECharacterData.LoadFromDict))]
+        class GdeCharactersPatch
+        {
+            static void Postfix(GDECharacterData __instance, Dictionary<string, object> dict)
+            {
+                if (__instance.Key == GDEItemKeys.Character_Prime)
+                {
+                    dict.TryGetVector2("MAXHP", out Vector2 ogMAXHP);
+                    __instance.MAXHP = new Vector2(29, ogMAXHP.y);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GDESkillData), nameof(GDESkillData.LoadFromDict))]
+        class GdeSkillPatch
+        {
+            static void Postfix(GDESkillData __instance, Dictionary<string, object> dict)
+            {
+                // high energy emission
+                if (__instance.Key == GDEItemKeys.Skill_S_Prime_13)
+                {
+                    __instance.NotCount = true;
+                }
+                // duel
+                else if (__instance.Key == GDEItemKeys.Skill_S_Prime_6)
+                {
+                    __instance.NotCount = false;
+                }
+                // absolute defense
+                else if (__instance.Key == GDEItemKeys.Skill_S_Prime_10)
+                {
+                    __instance.NoBasicSkill = true;
+                    __instance.UseAp = 1;
+                }
+                // double armor
+                else if (__instance.Key == GDEItemKeys.Skill_S_Prime_8)
+                {
+                    __instance.UseAp = 0;
+                }
+                // shield of retribution
+                else if (__instance.Key == GDEItemKeys.Skill_S_Prime_11)
+                {
+                    __instance.SkillExtended = new List<string>() { nameof(Extended_Ironheart_ShieldOfRetribution) };
+                    __instance.Description = CustomLoc.MainFile.GetTranslation(CustomLoc.TermKey(GDESchemaKeys.Skill, GDEItemKeys.Skill_S_Prime_11, CustomLoc.TermType.Description));
+
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GDESkillEffectData), nameof(GDESkillEffectData.LoadFromDict))]
+        class GdeSkillEffectPatch
+        {
+            static void Postfix(GDESkillEffectData __instance)
+            {
+                // charge of faith
+                if (__instance.Key == GDEItemKeys.SkillEffect_SE_Prime_12_T)
+                {
+                    __instance.Buffs = new List<GDEBuffData>() { new GDEBuffData(GDEItemKeys.Buff_B_Taunt), new GDEBuffData(GDEItemKeys.Buff_B_Taunt) };
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GDEBuffData), nameof(GDEBuffData.LoadFromDict))]
+        class GdeBuffPatch
+        {
+            static void Postfix(GDEBuffData __instance)
+            {
+                // shield of retribution
+                if (__instance.Key == GDEItemKeys.Buff_B_Prime_11_T)
+                {
+                    __instance.LifeTime = 3;
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ src/Character-rebalance/Extends/Extended_Ironhear_ShieldOfRetribution.cs
@@ -0,0 +1,65 @@
+﻿using GameDataEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Extended_Ironheart_ShieldOfRetribution : Skill_Extended, IP_SkillUse_Team
+{
+
+    public override string DescExtended(string desc)
+    {
+        return base.DescExtended(desc).Replace("&a", this.hitsLeft.ToString());
+    }
+    public override void HandInit()
+    {
+        base.HandInit();
+        hitsLeft = 4;
+    }
+
+
+    public override void SkillUseHand(BattleChar Target)
+    {
+        base.SkillUseHand(Target);
+        Debug.Log(Target?.Info.KeyData);
+        this.target = Target;
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        this.CountingExtedned = true;
+    }
+
+    public IEnumerator Attack()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        Skill Temp = Skill.TempSkill(GDEItemKeys.Skill_S_Prime_11, this.BChar, this.BChar.MyTeam);
+        Temp.PlusHit = true;
+        Temp.FreeUse = true;
+        if (!target.IsDead)
+        {
+            this.BChar.ParticleOut(this.MySkill, Temp, target);
+        }
+        else
+        {
+            this.BChar.ParticleOut(this.MySkill, Temp, BattleSystem.instance.EnemyList.Random<BattleEnemy>());
+        }
+        yield break;
+    }
+
+    public void SkillUseTeam(Skill skill)
+    {
+        if (this.hitsLeft >= 1 && this.MySkill.IsNowCounting && skill.IsHeal && BattleSystem.instance.EnemyList.Count != 0)
+        {
+            this.hitsLeft--;
+            BattleSystem.DelayInput(this.Attack());
+        }
+    }
+
+    BattleChar target;
+
+    int hitsLeft = 4;
+}
